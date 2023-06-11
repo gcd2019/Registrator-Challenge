2b89c36d309f44790f8011c5c91be3f952a690d1
Guillermo Castro <guillermo.castro.19@um.edu.mt>
2023-06-11 14:05:41 +0200

    Add basic structure of the project

---
+++ Registrator/Registrator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Registrator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
