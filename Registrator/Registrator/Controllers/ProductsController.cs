91c42605dbdbdf85c106c270674e04c723118836
Guillermo Castro <guillermo.castro.19@um.edu.mt>
2023-06-11 17:13:38 +0200

    Add products controller

---
+++ Registrator/Registrator/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Registrator.Models;
+
+namespace Registrator.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // This should be replaced by an actual database in a real-world app
+        private static List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product 1", Description = "Description 1", Price = 10},
+        };
+
+        public ActionResult Index()
+        {
+            return View(_products);
+        }
+    }
+}
