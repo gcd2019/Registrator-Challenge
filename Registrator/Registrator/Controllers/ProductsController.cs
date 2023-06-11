76c2996d35e70be33800e720280aa8e05e3ed677
Guillermo Castro <guillermo.castro.19@um.edu.mt>
2023-06-11 19:06:46 +0200

    Make the pages look nicer with bootstrap

--- Registrator/Registrator/Controllers/ProductsController.cs
+++ Registrator/Registrator/Controllers/ProductsController.cs
@@ -10,6 +10,7 @@ namespace Registrator.Controllers
         {
             new Product { Id = 1, Name = "Product 1", Description = "Description 1", Price = 5},
             new Product { Id = 2, Name = "Product 2", Description = "Description 2", Price = 10},
+            new Product { Id = 3, Name = "Product 3", Description = "Description 3", Price = 20},
         };
 
         public ActionResult Index()
